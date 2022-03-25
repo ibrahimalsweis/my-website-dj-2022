42264bace290b3415a556ad5fb79613b96fa55c5
BrahimAlSweis <83158760+ibrahimalsweis@users.noreply.github.com>
2022-03-25 17:55:44 +0300

    3 commit

---
+++ my_website_dj/my_website_dj/static/js/main.js
@@ -0,0 +1,25 @@
+// // icon menu
+// let comment_icon = document.querySelectorAll(".comment-icon");
+// let container_comments = document.querySelector('.box-comments-share')
+// let box_comments = document.querySelector('.box-comments ')
+// let close_box_comments = document.querySelector('.box-comments-share .box-comments .close')
+
+// comment_icon.forEach(function(e){
+//   e.addEventListener('click',function(){
+//     container_comments.classList.toggle('active')
+//     box_comments.classList.toggle('active')
+//   })
+// })
+
+// close_box_comments.addEventListener('click',function(){
+//   container_comments.classList.remove('active')
+//   box_comments.classList.remove('active')
+// })
+// window.onkeyup = function(event){
+//   if (container_comments.classList.contains('active')) {
+    
+//     if (event.key === 'Escape') {
+//       container_comments.classList.remove("active")
+//     }
+//   }
+// }
