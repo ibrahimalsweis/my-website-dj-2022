9c14f23c5f3e3bf36b8f2a58e7f48d6741fa1287
BrahimAlSweis <83158760+ibrahimalsweis@users.noreply.github.com>
2022-03-27 16:00:58 +0300

    27 /3/2022

--- my_website_dj/blog/urls.py
+++ my_website_dj/blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-        path('add_post/',views.new_post,name='new_post'),
+    path('add_post/',views.new_post,name='new_post'),
     path('detail/<int:post_id>/',views.detail_post,name='detail')
 ]
--- my_website_dj/user/views.py
+++ my_website_dj/user/views.py
@@ -1,11 +1,40 @@
-from multiprocessing import context
-from django.shortcuts import render
+
+from django.shortcuts import render ,redirect
+from django.contrib import messages
 from .forms import UserCreationForm
 # Create your views here.
 
 def register(request):
-    form =UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            messages.success(request,f'مرحباً {username} تم أنشاء الحساب بنجاح')
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+
     context={
         'form':form
     }
     return render(request,'user/register.html',context)
+
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             new_user = form.save(commit=False)
+#             username = form.cleaned_data['username']
+#             # new_user.set_password(form.cleaned_data['password1'])
+#             # new_user.save()
+
+#             messages.success(
+#                 request, f'تهانينا {new_user} لقد تمت عملية التسجيل بنجاح.')
+#             return redirect('login')
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'user/register.html', {
+#         'title': 'التسجيل',
+#         'form': form,
+#     })
